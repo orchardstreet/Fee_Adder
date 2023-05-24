c99c34633eba2fa58b4920a3d509f59f42513299
orchardstreet <xicardle@proton.me>
2023-05-24 00:04:07 -0400

    added totals row

--- fee_adder.c
+++ fee_adder.c
@@ -1,5 +1,7 @@
-/* TODO add payment method column */
 /* TODO add to bottom, two total label to add total of all, including unhidden */
+/* TODO create add_all_rows() function to update above
+ * to check how to do so in a memory safe way, check this https://github.com/hluk/sprinter-gtk/blob/master/main.c */
+/* TODO add payment method column */
 /* TODO add 'save', 'open' buttons */
 /* TODO less 0s https://docs.gtk.org/gtk3/treeview-tutorial.html#cell-data-functions */
 #include <stdlib.h>
@@ -11,15 +13,16 @@
 #include <math.h>
 #define MAX_ENTRIES 100000
 #define MAX_DATE_CHARS 9 
+#define MAX_METHOD_CHARS 20 /* adjustable */ 
 #define MAX_PERSON_CHARS 100 /* adjustable */
 #define MAX_INDIVIDUAL_FEE_NUMBER_CHARS 21
 
 /* enums */
 enum exit_codes {SUCCESS,FAILURE};
-enum columns {DATE_C, PERSON_C, AMOUNT_C, SHOW_C, TOTAL_COLUMNS};
+enum columns {DATE_C, PERSON_C, PAYMENT_METHOD_C, AMOUNT_C, SHOW_C, TOTAL_COLUMNS};
 
 /* Init global variables */
-GtkWidget *amount_entry, *date_entry, *person_entry;
+GtkWidget *amount_entry, *date_entry, *person_entry, *method_entry;
 GtkTextBuffer *error_buffer;
 GtkWidget *error_widget;
 GtkWidget *scrolled_window;
@@ -101,8 +104,19 @@ unsigned char validate_person(char *text)
 	if(!strlen(text)) {
 		gtk_text_buffer_set_text(error_buffer,"Please enter a person",-1);
 		return FAILURE;
-	} else if(strlen(text) > MAX_PERSON_CHARS - 1)  {
-		gtk_text_buffer_set_text(error_buffer,"Name of person is too long, recompile to allow longer person names",-1);
+	}else if(strlen(text) > MAX_PERSON_CHARS - 1)  {
+		gtk_text_buffer_set_text(error_buffer,"Name of person is too long, "
+				"recompile to allow longer person names",-1);
+		return FAILURE;
+	}
+	return SUCCESS;
+}
+
+unsigned char validate_method(char *text)
+{
+	if(strlen(text) > MAX_METHOD_CHARS - 1)  {
+		gtk_text_buffer_set_text(error_buffer,"Name of payment method is too long, "
+				"recompile to allow longer method names",-1);
 		return FAILURE;
 	}
 	return SUCCESS;
@@ -133,11 +147,15 @@ void do_add(GtkWidget *widget, gpointer model)
 	char *date_ptr = (char *)gtk_entry_get_text(GTK_ENTRY(date_entry));
 	char *person_ptr = (char *)gtk_entry_get_text(GTK_ENTRY(person_entry));
 	char *amount_ptr = (char *)gtk_entry_get_text(GTK_ENTRY(amount_entry));
+	char *method_ptr = (char *)gtk_entry_get_text(GTK_ENTRY(method_entry));
 
 	if(validate_date(date_ptr) == FAILURE) {
 		return;
 	}
 	if(validate_person(person_ptr) == FAILURE) {
+		return;
+	}
+	if(validate_method(method_ptr) == FAILURE) {
 		return;
 	}
 	if(str_to_double(amount_ptr,&number) == FAILURE)
@@ -147,6 +165,7 @@ void do_add(GtkWidget *widget, gpointer model)
 	gtk_list_store_insert_with_values(model, NULL, -1,
 					DATE_C, date_ptr,
 					PERSON_C, person_ptr,
+					PAYMENT_METHOD_C, method_ptr,
 					AMOUNT_C, number,
 					SHOW_C, 1, /* 1 for, yes show in tree */
 					-1);
@@ -155,6 +174,7 @@ void do_add(GtkWidget *widget, gpointer model)
 	gtk_entry_set_text(GTK_ENTRY(date_entry), "");
 	gtk_entry_set_text(GTK_ENTRY(person_entry), "");
 	gtk_entry_set_text(GTK_ENTRY(amount_entry), "");
+	gtk_entry_set_text(GTK_ENTRY(method_entry), "");
 
 }
 
@@ -166,9 +186,10 @@ int main(int argc, char **argv)
 	GtkWidget *grid, *grid2, *box, *box2, *date_label, *person_label, 
 		  *amount_label, *add_label, *tree_view;
 	GtkWidget *filter_button, *search_button, *edit_button, *add_button, 
-		*hide_button, *delete_button, *hide_all_button, *show_all_button, *save_button;
+		*hide_button, *delete_button, *hide_all_button, *show_all_button, *save_button,
+			*method_label;
 	GtkListStore *model;
-	GtkTreeViewColumn *column;
+	GtkTreeViewColumn *column, *column1, *column2, *column3;
 	GtkTreeModel *filter;
 
 	/* Init GTK */
@@ -185,11 +206,10 @@ int main(int argc, char **argv)
 	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 	gtk_window_set_title (GTK_WINDOW(window) , "Fee Adder" );
 	gtk_container_set_border_width (GTK_CONTAINER(window),10); 
-	gtk_window_set_default_size (GTK_WINDOW (window), 300, 500);
 	gtk_widget_add_events(window, GDK_KEY_PRESS_MASK);
 	g_signal_connect (G_OBJECT (window), "key_press_event",
         G_CALLBACK (keypress_function), NULL);
-	gtk_window_set_default_size ( GTK_WINDOW(window), 700, 480);
+	gtk_window_set_default_size ( GTK_WINDOW(window), 800, 580);
 
 	/* Destroy window on close */
 	g_signal_connect(window,"destroy",G_CALLBACK(gtk_main_quit),NULL);
@@ -205,6 +225,8 @@ int main(int argc, char **argv)
 
 	/* Add date entry to value entry grid */
 	date_label = gtk_label_new("Date (dd/mm/yy)");
+        gtk_widget_set_halign (date_label, GTK_ALIGN_START);
+	gtk_widget_set_margin_start(date_label,3);
 	gtk_grid_attach(GTK_GRID(grid),date_label,0,0,1,1);
 	date_entry = gtk_entry_new();
 	gtk_grid_attach(GTK_GRID(grid),date_entry,0,1,1,1);
@@ -212,42 +234,60 @@ int main(int argc, char **argv)
 
 	/* Add person entry to value entry grid */
 	person_label = gtk_label_new("Person");
+        gtk_widget_set_halign (person_label, GTK_ALIGN_START);
+	gtk_widget_set_margin_start(person_label,3);
 	gtk_grid_attach(GTK_GRID(grid),person_label,1,0,1,1);
 	person_entry = gtk_entry_new();
 	gtk_grid_attach(GTK_GRID(grid),person_entry,1,1,1,1);
 	gtk_entry_set_width_chars (GTK_ENTRY(person_entry), 20);
 
+	/* Add payment method to value entry grid */
+	method_label = gtk_label_new("Method");
+        gtk_widget_set_halign (method_label, GTK_ALIGN_START);
+	gtk_widget_set_margin_start(method_label,3);
+	gtk_grid_attach(GTK_GRID(grid),method_label,2,0,1,1);
+	method_entry = gtk_entry_new();
+	gtk_grid_attach(GTK_GRID(grid),method_entry,2,1,1,1);
+	gtk_entry_set_width_chars (GTK_ENTRY(method_entry), 10);
+
 	/* Add amount entry to value entry grid */
 	amount_label = gtk_label_new("Amount");
-	gtk_grid_attach(GTK_GRID(grid),amount_label,2,0,1,1);
+        gtk_widget_set_halign (amount_label, GTK_ALIGN_START);
+	gtk_widget_set_margin_start(amount_label,3);
+	gtk_grid_attach(GTK_GRID(grid),amount_label,3,0,1,1);
 	amount_entry = gtk_entry_new();
-	gtk_grid_attach(GTK_GRID(grid),amount_entry,2,1,1,1);
+	gtk_grid_attach(GTK_GRID(grid),amount_entry,3,1,1,1);
 	gtk_entry_set_width_chars (GTK_ENTRY(amount_entry), 10);
+	/* Align value entry grid */
+        gtk_widget_set_halign (grid, GTK_ALIGN_CENTER);
+	gtk_widget_set_margin_end(grid,45);
 
 	/* Add 'add' button to value entry grid */
 	add_label = gtk_label_new("");
-	gtk_grid_attach(GTK_GRID(grid),add_label,3,0,1,1);
+	gtk_grid_attach(GTK_GRID(grid),add_label,4,0,1,1);
 	add_button = gtk_button_new_with_label("Add");
-	gtk_grid_attach(GTK_GRID(grid), add_button, 3, 1, 1, 1);
-
-	/* Center value entry grid */
-        gtk_widget_set_halign (grid, GTK_ALIGN_CENTER);
+	gtk_grid_attach(GTK_GRID(grid), add_button, 4, 1, 1, 1);
 
 	/* Create horizontally oriented box to horizontally pack progam widgets into */
-	box2 = gtk_box_new(GTK_ORIENTATION_HORIZONTAL,70);
+	box2 = gtk_box_new(GTK_ORIENTATION_HORIZONTAL,20);
 	/* Pack horizontal box into vertical box */
 	gtk_box_pack_start (GTK_BOX (box), box2, FALSE, TRUE, 0);
 	/* Align horizontal box */
         gtk_widget_set_halign (box2, GTK_ALIGN_CENTER);
 
+	/* Create another vertically oriented box to pack into last hbox
+	 * this will contain the scrolled window and totals */
+	GtkWidget *box3 = gtk_box_new(GTK_ORIENTATION_VERTICAL,20);
+	gtk_box_pack_start (GTK_BOX (box2), box3, FALSE, TRUE, 0);
+
 	/* Create a scrollable window */
 	scrolled_window = gtk_scrolled_window_new(NULL, NULL);
 	/* Set preferred size of scrollable window */
- 	gtk_widget_set_size_request (scrolled_window, 340, 100);
+ 	gtk_widget_set_size_request (scrolled_window, 540, 300);
 	/* Always include a vertical scroll cursor on scroll window */
 	gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scrolled_window), GTK_POLICY_AUTOMATIC, GTK_POLICY_ALWAYS);
 	/* Add scrollable window to horizontal box */
-	gtk_box_pack_start (GTK_BOX (box2), scrolled_window, TRUE, TRUE, 0);
+	gtk_box_pack_start (GTK_BOX (box3), scrolled_window, TRUE, TRUE, 0);
 	/* Align scrollable window */
         gtk_widget_set_halign (scrolled_window, GTK_ALIGN_START);
 	g_object_set (scrolled_window, "vexpand", TRUE, NULL);
@@ -259,9 +299,10 @@ int main(int argc, char **argv)
 	/* Create liststore for table */
 	model = gtk_list_store_new(
 		TOTAL_COLUMNS,  /* required parameter, total columns */
-		G_TYPE_STRING,  /* Second column, Date, DATE_C */
-		G_TYPE_STRING,  /* Third column, Person, PERSON_C */
-		G_TYPE_DOUBLE,  /* Fourth column, AMOUNT, AMOUNT_C */
+		G_TYPE_STRING,  /* Second column, date, DATE_C */
+		G_TYPE_STRING,  /* Third column, person, PERSON_C */
+		G_TYPE_STRING,  /* Fourth column, payment method, PAYMENT_METHOD_C */
+		G_TYPE_DOUBLE,  /* Fifth column, amount, AMOUNT_C */
 		G_TYPE_BOOLEAN  /* 1 for show, 0 for hide */
 		);
 
@@ -276,7 +317,7 @@ int main(int argc, char **argv)
 
 	/* create treestore as filter of liststore */
 	filter = gtk_tree_model_filter_new(GTK_TREE_MODEL(model),NULL);
-	gtk_tree_model_filter_set_visible_column(GTK_TREE_MODEL_FILTER(filter),3);
+	gtk_tree_model_filter_set_visible_column(GTK_TREE_MODEL_FILTER(filter),TOTAL_COLUMNS - 1);
 
 	/* Create treeview from treestore */
 	tree_view = gtk_tree_view_new_with_model(GTK_TREE_MODEL(filter));
@@ -295,24 +336,42 @@ int main(int argc, char **argv)
 		"text", DATE_C,
 		NULL);
 	gtk_tree_view_append_column(GTK_TREE_VIEW(tree_view), column);
+	gtk_tree_view_column_set_expand ( column, TRUE);
+	gtk_tree_view_column_set_fixed_width (column,125);
 
 	/* Treeview column 1 */
-	column = gtk_tree_view_column_new_with_attributes("Person",
+	column1 = gtk_tree_view_column_new_with_attributes("Person",
 		gtk_cell_renderer_text_new(),
 		"text", PERSON_C,
 		NULL);
-	gtk_tree_view_append_column(GTK_TREE_VIEW(tree_view), column);
+	gtk_tree_view_append_column(GTK_TREE_VIEW(tree_view), column1);
+	gtk_tree_view_column_set_expand ( column1, TRUE);
+	gtk_tree_view_column_set_fixed_width (column1,190);
 
 	/* Treeview column 2 */
-	column = gtk_tree_view_column_new_with_attributes("Amount",
+	column2 = gtk_tree_view_column_new_with_attributes("Method",
+		gtk_cell_renderer_text_new(),
+		"text", PAYMENT_METHOD_C,
+		NULL);
+	gtk_tree_view_append_column(GTK_TREE_VIEW(tree_view), column2);
+	gtk_tree_view_column_set_expand ( column2, TRUE);
+	gtk_tree_view_column_set_fixed_width (column2,105);
+
+
+	/* Treeview column 3 */
+	column3 = gtk_tree_view_column_new_with_attributes("Amount",
 		gtk_cell_renderer_text_new(),
 		"text", AMOUNT_C,
 		NULL);
-	gtk_tree_view_append_column(GTK_TREE_VIEW(tree_view), column);
+	gtk_tree_view_append_column(GTK_TREE_VIEW(tree_view), column3);
+	gtk_tree_view_column_set_expand ( column3, TRUE);
+	gtk_tree_view_column_set_fixed_width (column3,100);
+
 
 	/* Create grid for right-side buttons */
 	grid2 = gtk_grid_new();
 	gtk_grid_set_row_spacing (GTK_GRID(grid2),20);
+	gtk_widget_set_margin_top(grid2,55);
 
 	/* Create filter button */
 	filter_button = gtk_button_new_with_label("Filter");
@@ -351,7 +410,39 @@ int main(int argc, char **argv)
 
 	/* Align grid2 */
         gtk_widget_set_halign (grid2, GTK_ALIGN_START);
-        gtk_widget_set_valign (grid2, GTK_ALIGN_CENTER);
+        gtk_widget_set_valign (grid2, GTK_ALIGN_START);
+
+	/* Create grid for totals */
+	GtkWidget *grid3, *filters_applied_label, *filters_applied_results_label, *total_filtered_label,
+		  *total_filtered_results_label, *total_label, *total_results_label;
+	grid3 = gtk_grid_new();
+	/* Set grid spacing */
+	gtk_grid_set_row_spacing (GTK_GRID(grid3),9);
+	gtk_grid_set_column_spacing (GTK_GRID(grid3),6);
+	/* Set grid alignment */
+        gtk_widget_set_halign (grid3, GTK_ALIGN_END);
+	gtk_widget_set_margin_end(grid3,30);
+	/* Create grid members */
+	//filters_applied_label = gtk_label_new("Filters applied: ");
+        //gtk_widget_set_halign (filters_applied_label, GTK_ALIGN_START);
+	//filters_applied_results_label = gtk_label_new("date, payment method");
+        //gtk_widget_set_halign (filters_applied_results_label, GTK_ALIGN_START);
+	total_filtered_label = gtk_label_new("Total of filtered fees: ");
+        gtk_widget_set_halign (total_filtered_label, GTK_ALIGN_END);
+	total_filtered_results_label = gtk_label_new("40000");
+        gtk_widget_set_halign (total_filtered_results_label, GTK_ALIGN_END);
+	total_label = gtk_label_new("Total fees: ");
+        gtk_widget_set_halign (total_label, GTK_ALIGN_END);
+	total_results_label = gtk_label_new("50000");
+        gtk_widget_set_halign (total_results_label, GTK_ALIGN_END);
+	/* Attach grid members */
+	//gtk_grid_attach(GTK_GRID(grid3),filters_applied_label,0,0,1,1);
+	//gtk_grid_attach(GTK_GRID(grid3),filters_applied_results_label,1,0,3,1);
+	gtk_grid_attach(GTK_GRID(grid3),total_filtered_label,0,1,1,1);
+	gtk_grid_attach(GTK_GRID(grid3),total_filtered_results_label,1,1,1,1);
+	gtk_grid_attach(GTK_GRID(grid3),total_label,0,2,1,1);
+	gtk_grid_attach(GTK_GRID(grid3),total_results_label,1,2,1,1);
+	gtk_box_pack_start (GTK_BOX (box3), grid3, TRUE, TRUE, 0);
 
 	/* error widget */
 	error_buffer = gtk_text_buffer_new(NULL);
