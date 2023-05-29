65cdcadd5af543589800a8dcc2032b14207454a1
Mamoop Ohpoo <39713147+orchardstreet@users.noreply.github.com>
2023-05-28 18:19:06 -0700

    Update build-linux.sh

--- build-linux.sh
+++ build-linux.sh
@@ -1,5 +1,3 @@
 #! /bin/bash
 
-#sudo apt install libgtk-3-dev
-
-gcc fee_adder.c -o fee_adder `pkg-config --cflags gtk+-3.0` `pkg-config --libs gtk+-3.0`
+gcc src/fee_adder.c -o fee_adder `pkg-config --cflags gtk+-3.0` `pkg-config --libs gtk+-3.0`
